54caa5d1d1023d3a6ae9ba411be275f1e2966740
tanishapatil <tanishapatil07@gmail.com>
2022-10-27 18:14:42 -0700

    musicchanges

--- model_music.py
+++ model_music.py
@@ -1,29 +1,30 @@
-def music_function(): # define whole function
-    import requests
-    artist = input("Enter artist name:") # allow user to input the artist they want
 
-    url = "https://youtube-music1.p.rapidapi.com/v2/search"
+import requests
+artist = input("Enter artist name:") # allow user to input the artist they want
 
-    querystring = {"query":artist}
+url = "https://youtube-music1.p.rapidapi.com/v2/search"
 
-    headers = {
+querystring = {"query":artist}
+
+headers = {
 	    "X-RapidAPI-Key": "4a1e45ca8dmshcde31ef8a5baed3p1a2a7ejsn9952d2cf96bd", # use personal key
 	    "X-RapidAPI-Host": "youtube-music1.p.rapidapi.com"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-    print(response.text) # print all data that is in API
+# print(response.text) # print all data that is in API
 
 
-    import json # using .json to reorganize raw data
+import json # using .json to reorganize raw data
 
     # define variables and assign a value
-    string_val = response.text
-    dict_val = json.loads(string_val)
-    album_list = [] # create a new list to start from scratch
-    songlist = dict_val["result"]["songs"] # dictionary values from API dictionary
+string_val = response.text
+dict_val = json.loads(string_val)
+album_list = [] # create a new list to start from scratch
+songlist = dict_val["result"]["songs"] # dictionary values from API dictionary
 
+def music_function(): # define whole function
     print("Artist's Albums:")
     for song in songlist:
         if song["album"]["name"] not in album_list: 
@@ -37,6 +38,7 @@ def music_function(): # define whole function
     found = False # by default nothing is found
     for song in songlist:
         if song["album"]["name"] == album: # if machine is able to find song name in dictionary then song names will be printed
+            print("Song's in " + album)
             print(song["name"])
             found = True # variable is now set as true
 
