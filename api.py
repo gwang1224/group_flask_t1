cfa716459089648feb65701f150fd7c79ec11100
gwang1224 <gracewang187@gmail.com>
2022-11-01 19:45:31 -0700

    /api/music/taylor

--- api.py
+++ api.py
@@ -3,102 +3,18 @@ from flask_restful import Api, Resource # used for REST API building
 import requests  # used for testing 
 import random
 
-from model_music import *
+from model_jokes import *
+from model_music import initAPI
 
 app_api = Blueprint('api', __name__,
-                   url_prefix='/api/jokes')
+                   url_prefix='/api/music')
 
 # # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(app_api)
 
-# class JokesAPI:
-#     # not implemented
-#     class _Create(Resource):
-#         def post(self, joke):
-#             pass
-            
-#     # getJokes()
-#     class _Read(Resource):
-#         def get(self):
-#             return jsonify(getJokes())
+class TaylorAPI:
+    class _Taylor(Resource):
+        def get(self):
+            return jsonify(initAPI())
 
-#     # getJoke(id)
-#     class _ReadID(Resource):
-#         def get(self, id):
-#             return jsonify(getJoke(id))
-
-#     # getRandomJoke()
-#     class _ReadRandom(Resource):
-#         def get(self):
-#             return jsonify(getRandomJoke())
-    
-#     # getRandomJoke()
-#     class _ReadCount(Resource):
-#         def get(self):
-#             count = countJokes()
-#             countMsg = {'count': count}
-#             return jsonify(countMsg)
-
-#     # put method: addJokeHaHa
-#     class _UpdateLike(Resource):
-#         def put(self, id):
-#             addJokeHaHa(id)
-#             return jsonify(getJoke(id))
-
-    # put method: addJokeBooHoo
-    # class _UpdateJeer(Resource):
-    #     def put(self, id):
-    #         addJokeBooHoo(id)
-    #         return jsonify(getJoke(id))
-
-    # # building RESTapi resources/interfaces, these routes are added to Web Server
-    # api.add_resource(_Create, '/create/<string:joke>')
-    # api.add_resource(_Read, '/')
-    # api.add_resource(_ReadID, '/<int:id>')
-    # api.add_resource(_ReadRandom, '/random')
-    # api.add_resource(_ReadCount, '/count')
-    # api.add_resource(_UpdateLike, '/like/<int:id>')
-    # api.add_resource(_UpdateJeer, '/jeer/<int:id>')
-    
-if __name__ == "__main__": 
-    server = "http://127.0.0.1:5000" # run local
-    #server = 'https://musicmania.nighthawkcodescrums.gq' # run from web
-    url = server + "/api/jokes"
-    # responses = []  # responses list
-
-    #Added by Grace
-    artist = input("Who is your favorite artist?")
-    print(initAPI(artist))
-
-
-    # # get count of jokes on server
-    # count_response = requests.get(url+"/count")
-    # count_json = count_response.json()
-    # count = count_json['count']
-
-    # # update likes/dislikes test sequence
-    # num = str(random.randint(0, count-1)) # test a random record
-    # responses.append(
-    #     requests.get(url+"/"+num)  # read joke by id
-    #     ) 
-    # responses.append(
-    #     requests.put(url+"/like/"+num) # add to like count
-    #     ) 
-    # responses.append(
-    #     requests.put(url+"/jeer/"+num) # add to jeer count
-    #     ) 
-
-    # # obtain a random joke
-    # responses.append(
-    #     requests.get(url+"/random")  # read a random joke
-    #     ) 
-
-    # # cycle through responses
-    # for response in responses:
-    #     print(response)
-    #     print(artist)
-    #     try:
-    #         print(response.json())
-
-    #     except:
-    #         print("unknown error")
+    api.add_resource(_Taylor, '/taylor')
