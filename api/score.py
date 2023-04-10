8b71966eeaea5614bb2a5ae160ec16a66df8738a
Grace Wang <94924172+gwang1224@users.noreply.github.com>
2023-04-09 20:13:09 -0700

    Update score.py

--- api/score.py
+++ api/score.py
@@ -1,27 +1,26 @@
 import json
 from flask import Blueprint, request, jsonify
-from flask_restful import Api, Resource # used for REST API building
-
-# from model.users import User
+from flask_restful import Api, Resource
 from model.scores import Score
 
+# Creates Blueprint called score_api and establishes url directory
 score_api = Blueprint('score_api', __name__,
                    url_prefix='/api/scores')
 
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(score_api)
 
 class ScoreAPI:        
     class _Create(Resource):
         def post(self):
-            ''' Read data for json body '''
+            # Parses the incoming JSON request data and returns it
             body = request.get_json()
             
-            ''' Avoid garbage in, error checking '''
-            # validate name
+            # Checks user inputs, reduce unnecessary data with error checking
             name = body.get('name')
+            # If user input of name is less than 2 character, returns 400 error
             if name is None or len(name) < 2:
                 return {'message': f'Name is missing, or is less than 2 characters'}, 400
+              
             # validate uid
             uid = body.get('uid')
             if uid is None or len(uid) < 1:
